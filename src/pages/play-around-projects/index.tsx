3be78d737a80bd64fcfb942cbcc0ab65a0fea9c0
TimDWalter <tim.d.walter@web.de>
2024-08-11 01:05:37 +0200

    fix shadow in projects during transition

--- src/pages/play-around-projects/index.tsx
+++ src/pages/play-around-projects/index.tsx
@@ -22,9 +22,9 @@ export default function Home() {
           className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px]
             before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent
             before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px]
-            after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl
-            after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10
-            after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]"
+            after:translate-x-1/3
+            after:content-[''] 
+            "
         >
           <ul className="honeycomb wrapper-container">
             <li className="honeycomb-cell">
@@ -140,6 +140,44 @@ export default function Home() {
                 />
                 <div className="honeycomb-cell__title">
                   In Progress<small>Just a second...</small>
+                </div>
+              </Link>
+            </li>
+            <li className="honeycomb-cell honeycomb__placeholder"></li>
+            <li className="honeycomb-cell">
+              <Link
+                href="https://example.com/coming-up-soon"
+                className={"flex-auto"}
+              >
+                <Image
+                  className="honeycomb-cell__image"
+                  src="/Not_Found.png"
+                  width={500}
+                  height={500}
+                  alt="In Progress"
+                  priority
+                />
+                <div className="honeycomb-cell__title">
+                  In Progress<small>Just a tiny second...</small>
+                </div>
+              </Link>
+            </li>
+            <li className="honeycomb-cell honeycomb__placeholder"></li>
+            <li className="honeycomb-cell">
+              <Link
+                href="https://example.com/coming-up-soon"
+                className={"flex-auto"}
+              >
+                <Image
+                  className="honeycomb-cell__image"
+                  src="/Not_Found.png"
+                  width={500}
+                  height={500}
+                  alt="In Progress"
+                  priority
+                />
+                <div className="honeycomb-cell__title">
+                  In Progress<small>...</small>
                 </div>
               </Link>
             </li>
